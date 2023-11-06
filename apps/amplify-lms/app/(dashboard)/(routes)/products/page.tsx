1782b822ba23a65dcbb6f60af99624a397560251
alexelizaga <aelizag@corp.vodafone.es>
2023-11-06 11:48:40 +0100

    feat: try api

--- apps/amplify-lms/app/(dashboard)/(routes)/products/page.tsx
+++ apps/amplify-lms/app/(dashboard)/(routes)/products/page.tsx
@@ -1,10 +1,14 @@
 import { WithAuthenticator } from '@/amplify-lms/components';
 import ProductsPage from './_components/ProductsPage';
 
-import getProducts from '@/amplify-lms/actions/getProducts';
+import axios from 'axios';
+import { Product } from '@/amplify-lms/API';
 
 export default async function Products() {
-  const products = await getProducts();
+  // const products = await getProducts();
+  const { data: products } = await axios.get<Product[]>(
+    'http://localhost:3000/api/products'
+  );
 
   return (
     <WithAuthenticator>
