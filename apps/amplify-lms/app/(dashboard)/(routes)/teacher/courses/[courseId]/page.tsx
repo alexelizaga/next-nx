6ce8678d131c07159261549438fb7ed859328ba0
alexelizaga <alexelizaga@icloud.com>
2023-11-10 13:43:38 +0100

    try: router push

--- apps/amplify-lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ apps/amplify-lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -7,23 +7,24 @@ import { ListCoursesQuery, ListCoursesQueryVariables } from '@/amplify-lms/API';
 import * as queries from '@/amplify-lms/graphql/queries';
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
-  const variables: ListCoursesQueryVariables = {
-    limit: 1,
-    filter: {
-      and: [{ id: { eq: params.courseId } }]
-    }
-  };
+  // const variables: ListCoursesQueryVariables = {
+  //   limit: 1,
+  //   filter: {
+  //     and: [{ id: { eq: params.courseId } }]
+  //   }
+  // };
 
-  const { data } = await API.graphql<GraphQLQuery<ListCoursesQuery>>({
-    query: queries.listCourses,
-    variables
-  });
+  // const { data } = await API.graphql<GraphQLQuery<ListCoursesQuery>>({
+  //   query: queries.listCourses,
+  //   variables
+  // });
 
-  if (!data?.listCourses?.items.length) {
-    return redirect('/');
-  }
+  // if (!data?.listCourses?.items.length) {
+  //   return redirect('/');
+  // }
 
-  return <div>ID: {data?.listCourses?.items[0]?.id}</div>;
+  // return <div>ID: {data?.listCourses?.items[0]?.id}</div>;
+  return <div>Hola mundo</div>;
 };
 
 export default CourseIdPage;
